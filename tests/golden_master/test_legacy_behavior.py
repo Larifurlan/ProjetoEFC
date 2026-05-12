18e5f30959a80ded7564a4264da9fccf752628ad
Maria Gabriella Xavier Puccinelli <mgabriella.puccinelli@gmail.com>
2026-05-12 14:09:47 -0300

    test: ampliar golden master tests e preservar comportamento legado

--- tests/golden_master/test_legacy_behavior.py
+++ tests/golden_master/test_legacy_behavior.py
@@ -27,3 +27,52 @@ def test_pedido_normal_calcula_total_corretamente(sis):
 
     assert pedido['tot'] == pytest.approx(245.0)
     assert pedido['st'] == 'pendente'
+
+def test_pedido_vip_aplica_desconto_de_5_por_cento(sis):
+    itens = [
+        {'nome': 'produto1', 'p': 100, 'q': 1, 'tipo': 'normal'}
+    ]
+
+    id_ped = sis.add_ped('Maria', itens, 'vip')
+
+    pedido = sis.get_ped(id_ped)
+
+    assert pedido['tot'] == pytest.approx(95.0)
+
+
+def test_pagamento_insuficiente_falha(sis):
+    itens = [
+        {'nome': 'produto1', 'p': 100, 'q': 1, 'tipo': 'normal'}
+    ]
+
+    id_ped = sis.add_ped('Joao', itens, 'normal')
+
+    assert sis.proc_pag(id_ped, 'cartao', 50) is False
+
+
+def test_pix_aprova_pedido_automaticamente(sis):
+    itens = [
+        {'nome': 'produto1', 'p': 100, 'q': 1, 'tipo': 'normal'}
+    ]
+
+    id_ped = sis.add_ped('Joao', itens, 'normal')
+
+    sis.proc_pag(id_ped, 'pix', 100)
+
+    pedido = sis.get_ped(id_ped)
+
+    assert pedido['st'] == 'aprovado'
+
+
+def test_boleto_nao_aprova_automaticamente(sis):
+    itens = [
+        {'nome': 'produto1', 'p': 100, 'q': 1, 'tipo': 'normal'}
+    ]
+
+    id_ped = sis.add_ped('Joao', itens, 'normal')
+
+    sis.proc_pag(id_ped, 'boleto', 100)
+
+    pedido = sis.get_ped(id_ped)
+
+    assert pedido['st'] == 'pendente'
